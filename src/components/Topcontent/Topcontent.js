a24df47cb58cd1a1ef898be3f91b85ade2bdcb9a
SudharsonKR <sudharsonkr@gmail.com>
2024-01-02 11:48:54 +0530

    Resume Update

--- src/components/Topcontent/Topcontent.js
+++ src/components/Topcontent/Topcontent.js
@@ -8,7 +8,7 @@ const Topcontent = () => {
         <div className='topcontent_container'>
 <h1>Mr. K. R. Sudharson</h1>
 <p>Full Stack Developer (MERN)</p>
-<a href="https://drive.google.com/file/d/1hW1DeyOpThy6r5wMiWP3bEhEv1q5E0Vx/view?usp=drive_link" target='_blank' rel="noreferrer">
+<a href="https://drive.google.com/file/d/1rbfC0--x1_PpPuplba_YfffYcQ-Mgj4k/view?usp=drive_link" target='_blank' rel="noreferrer">
     <button className='downloadbtn'>Download CV</button>
 </a>
 <Link to='projects' smooth={true} duration={500}>
