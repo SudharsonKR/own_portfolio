48b76dc2a80609ae19a1b353234930290a950566
SudharsonKR <sudharsonkr@gmail.com>
2024-01-25 18:49:49 +0530

    responsive Commit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,6 +3,7 @@ import { Element } from 'react-scroll'
 
 import './Contact.css'
 import linkedinimg from '../../Assets/LinkedIn.png'
+import gitimg from '../../Assets/Git.png'
 
 const Contact = () => {
   return (
@@ -12,6 +13,9 @@ const Contact = () => {
         <p>Email : <span style={{color:"yellow"}}>sudharsonkr@gmail.com</span></p>
         <p>Github User Name: <span style={{color:"yellow"}}>SudharsonKR</span></p>
         <div className='contact_link'>
+            <a href="https://github.com/SudharsonKR/" target='_blank' rel="noreferrer">
+                <img src={gitimg} alt=""/>
+            </a>
             <a href="https://www.linkedin.com/in/sudharson-k-r-8b5a88252/" target='_blank' rel="noreferrer">
                 <img src={linkedinimg} alt=""/>
             </a>
